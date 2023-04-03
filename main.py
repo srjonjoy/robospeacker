583029de220d1f8b5875199abc0b6a847d259fc5
srjonjoy <120779838+srjonjoy@users.noreply.github.com>
2023-04-03 06:39:44 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+if __name__ == '__main__':
+    print("welcome to robospeacker 1.0 , created by srinjoy")
+    x=input("enter what you want me to speak")
+    command=f"say {x}"
+    os.system(command)
+
+
